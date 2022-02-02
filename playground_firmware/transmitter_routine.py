d5ba9ce4fa9c465e03a2f9d80847e7e6d9e37888
ultimodruido <public@nicolinux.it>
2022-02-02 21:16:01 +0100

    transmitter_routine.py: updated to fit v 2.0 of protocol

--- playground_firmware/transmitter_routine.py
+++ playground_firmware/transmitter_routine.py
@@ -40,20 +40,16 @@ while True:
         try:
             if (value[0] == "C") & (value[-1] == "!"):
                 led.value = True
-                cmd = value[1:-1].split('.')
-                if cmd[0] == "__":
-                    pulses = get_pulses_length(cmd[1])
-                    transmit_array = array('H', 3 * pulses)
-                else:
-                    pulses_a = get_pulses_length(cmd[0])
-                    pulses_b = get_pulses_length(cmd[1])
-                    transmit_array = array('H', 3 * pulses_a + 2 * pulses_b)
-                # print(f"Array prepared: {transmit_array}")
+                cmd = value[1:-1]
+
+                pulses = get_pulses_length(cmd)
+                transmit_array = array('H', 3 * pulses)
+
                 ir_send(transmit_array)
-                print(f"{value}_OK\r")
+
                 led.value = False
 
         except Exception as e:
             print(f"{value}_ERROR\r")
 
-    sleep(0.3)
+    sleep(0.1)
