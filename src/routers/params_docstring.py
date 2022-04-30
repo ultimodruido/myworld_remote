01d98a9bffe2a985fb8f745459b35e64b594820c
ultimodruido <public@nicolinux.it>
2022-04-30 21:48:41 +0200

    Add selection Enum for speed_value path parameter
     -Data validation for the speed_value now in place
     -Live doc now shows the exact options available
     - Live doc now offer a dropdown selection for the live testing

--- src/routers/params_docstring.py
+++ src/routers/params_docstring.py
@@ -6,7 +6,7 @@ Avoids duplication of code or inconsistencies in the description as it appears i
 """
 param_doc = {
     "train_id":     "ID number of the train as results from the */train_list* command",
-    "speed_value":  "Speed code, to be selected from the list STOP, F1, F2, F3, B1, B2, B3",
+    "speed_value":  "Speed command code",
     "port_name":    "Name of the serial port, example 'COM3', used for the communication with the circuit playground",
     "train_name":   "name to be used by the UI to identify the train",
     "frequency":    "the frequency used to transmit commands to the train",
--- src/routers/train.py
+++ src/routers/train.py
@@ -3,6 +3,7 @@ Module: router - trains
 manage all endpoints related to train control
 """
 from fastapi import APIRouter, Request, Path
+from enum import Enum
 
 from server_deps import rolling_stock, reply, Message
 from server_lock import data_protection_lock
@@ -48,10 +49,21 @@ async def train_list(request: Request):
     return reply(request.url.path, True, train_list=rolling_stock.get_train_list())
 
 
+class ModelSpeedValue(str, Enum):
+    """Validation model for speed_value parameter"""
+    STOP = "STOP"
+    F1 = "F1"
+    F2 = "F2"
+    F3 = "F3"
+    B1 = "B1"
+    B2 = "B2"
+    B3 = "B3"
+
+
 @router.post("/train/{train_id}/speed/{speed_value}", response_model=Message)
 async def set_train_speed(request: Request,
                           train_id: int = Path(..., description=param_doc['train_id']),
-                          speed_value: str = Path(..., description=param_doc['speed_value'])
+                          speed_value: ModelSpeedValue = Path(..., description=param_doc['speed_value'])
                           ):
     """
     Change speed of a train
