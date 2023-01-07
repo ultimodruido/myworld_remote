6e9e125c4f2e422563e1009ba87434e17ca4ba26
ultimodruido <public@nicolinux.it>
2023-01-07 16:26:04 +0100

    Added freqs U & V
     - protocol.py: correction of frequencies dictionary
     - protocol.py: update of documentation

--- src/submodules/protocol.py
+++ src/submodules/protocol.py
@@ -5,11 +5,11 @@ Protocol
 Contains the binary codes conversion for each command given by the MyWorld remote.
 
 Each remote has 8 speed commands: STOP, 3 forward speed and 3 backward speed and
-emergency stop which seems to be recognized by every frequency. ::
+emergency stop which seems to be recognized by frequency pairs (ex. G & H). ::
 
     SPEED_SIGNALS = ['STOP', 'F1', 'F2', 'F3', 'B1', 'B2', 'B3', 'SOS']
 
-In addition there are functionalities like lights, horn and 2 sounds.
+In addition, there are functionalities like lights, horn and 2 sounds.
 An *empty* value is also needed to properly describe all possibilities. ::
 
     FUNCTION_SIGNALS = ['NO_FN', 'LIGHT', 'SOUND1', 'HORN', 'SOUND2']
@@ -22,22 +22,24 @@ The IR protocol pulses length used by Maerklin are as follow::
     bit 1 -> 500 + 1500
 
 .. WARNING ::
-    At the moment only frequencies G and H have been decoded.
+    At the moment only frequencies G, H, U and V have been decoded.
 
-A signal is 14 bits long, each signal is divided into 2 sequences of 7.
+A signal is between 14 and 18 bits long, each signal is divided into 2 sequences of equal length.
 The second sequence is just like the first one, but with each bit inverted.
 In the following code snippets the meaning of each bit will be explained.
 
-.. data:: bit [1]: frequency selector, obtained by the FREQ dictionary
+.. data:: 1 to 5 bits: frequency selector, obtained by the FREQ dictionary
 
 ::
 
-    FREQ = {
-        'G': 0,
-        'H': 1,
-    }
+FREQ = {
+    'G': '0',
+    'H': '1',
+    'U': '00110',
+    'V': '00111',
+}
 
-.. data:: bits [2:4]: speed selector, obtained by the SPEED dictionary
+.. data:: 3 bits: speed selector, obtained by the SPEED dictionary
 
 ::
 
@@ -52,7 +54,7 @@ In the following code snippets the meaning of each bit will be explained.
             'SOS': '111',
         }
 
-.. data:: bits [5:7]: function selector, obtained by the FUNCTION dictionary
+.. data:: 3 bits: function selector, obtained by the FUNCTION dictionary
 
 ::
 
@@ -69,7 +71,7 @@ To recreate the STOP signal for protocol H without function::
     code = f"{FREQ['H']}{SPEED['STOP']}{FUNCTION['NO_FN']}"
     code = 1 000 000
 
-we need to add the bit inverted ::
+we need to add the second sequence with inverted bits ::
 
     neg_code = 0 111 111
 
@@ -78,7 +80,7 @@ and the result is ::
     transmission_code = 1 000 000   0 111 111
 
 the signal starts with a pulse 4000 long and then uses the conversion explained
-at the top of the page to get the pulses length:
+at the top of the page to get the pulses' length:
 
 * bit 1 -> 500 + 1500
 * 7 times bit 0 -> 500 + 500
@@ -102,8 +104,10 @@ FUNCTION_SIGNALS = ['NO_FN', 'LIGHT', 'SOUND1', 'HORN', 'SOUND2']
 
 
 FREQ = {
-    'G': 0,
-    'H': 1,
+    'G': '0',
+    'H': '1',
+    'U': '00110',
+    'V': '00111',
 }
 
 SPEED = {
